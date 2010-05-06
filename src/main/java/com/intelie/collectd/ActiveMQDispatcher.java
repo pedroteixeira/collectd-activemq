b4e16ad9ae5b64e2060e9b58a96a996f055b6311
Pedro Teixeira <pedro.t@gmail.com>
2010-05-06 13:32:39 -0300

    working in progress first commit

---
+++ src/main/java/com/intelie/collectd/ActiveMQDispatcher.java
@@ -0,0 +1,11 @@
+package com.intelie.collectd;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: pedro
+ * Date: May 6, 2010
+ * Time: 12:52:14 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ActiveMQDispatcher {
+}
